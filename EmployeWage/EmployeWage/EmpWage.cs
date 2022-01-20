b3200c95f15f1e03ac93e4c692f15e4199042817
Vimagadum <95463195+Vimagadum@users.noreply.github.com>
2022-01-20 19:19:27 +0530

    [Vinayak] Checking emp is present or not

---
+++ EmployeWage/EmployeWage/EmpWage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeWage
+{
+    public class EmpWage
+    {
+        public static void IffElse()
+        {
+            Random random = new Random();
+
+            int RandomNumber = random.Next(0, 2);
+
+
+            if (RandomNumber == 0)
+            {
+                Console.WriteLine("Emplyee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Emplyee is absent");
+            }
+
+        }
+    }
+}
